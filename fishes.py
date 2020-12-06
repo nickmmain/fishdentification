e226968df62cbda89b0a25ec0d5dd408f2fcb337
nickmmain <nickm.main@gmail.com>
2020-12-05 22:13:39 -0500

    paths of masks

--- fishes.py
+++ fishes.py
@@ -1,5 +1,8 @@
 import cv2
 import os
+import re
+import sys
+
 
 # fish dataset from: http://groups.inf.ed.ac.uk/f4k/GROUNDTRUTH/RECOG/
 
@@ -11,4 +14,31 @@ def test_image(gray=True):
     return img
 
 
-def trainingImages(gray=True):
+# def trainingImages(gray=True, trainingPortion=0.7):
+
+
+def trainingMasks(trainingPortion=0.7, fish='all'):
+    dataPath = os.path.join(os.getcwd(), 'data')
+    maskFolders = []
+
+    for (dirpath, dirnames, filenames) in os.walk(dataPath):
+        for dir in dirnames:
+            masks = re.findall("^mask.*", dir)
+            if(len(masks) != 0):
+                maskFolders.append(os.path.join(dirpath, masks[0]))
+
+    trainingMaskPaths = []
+    for maskFolder in maskFolders:
+        maskPaths = []
+        for (dirpath, dirnames, filenames) in os.walk(maskFolder):
+            for picture in filenames:
+                maskPaths.append(os.path.join(dirpath, picture))
+
+        trainingMaskPaths.append(maskPaths)
+
+    return trainingMaskPaths
+
+
+if __name__ == "__main__":
+    trainingMasksArr = trainingMasks()
+    print("Whatup")
