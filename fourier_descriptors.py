54fde3146529e5ed9622a69ec8c2c3a750ae010e
nickmmain <nickm.main@gmail.com>
2020-12-08 09:31:52 -0500

    fourier descriptors feature

--- fourier_descriptors.py
+++ fourier_descriptors.py
@@ -4,14 +4,26 @@ from fishes import masks
 
 # the images of this data set are very small. The entire contour can be made up of 80 pixels.
 numContourPixelsToKeep = 20
+numfourierDescriptorsToKeep = 8
 
 
 def fourier_descriptors_feature(mask):
-    # trace the boundary of the bw image:
     contours, hierarchy = cv2.findContours(
-        mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
     if(len(contours) > 1):
-        raise TypeError
+        # if the masks are properly done, with RETR_EXTERNAL, this code shouldn't be executed.
+        colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
+        draw_on_these = cv2.cvtColor(np.copy(mask), cv2.COLOR_GRAY2BGR)
+
+        for i in range(len(contours)):
+            draw_on_these = cv2.drawContours(
+                draw_on_these, contours[i], -1, colors[i % 3], 1)
+
+        window_name = 'more than 1 contour !'
+        cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+        cv2.imshow(window_name, draw_on_these)
+        cv2.waitKey()
 
     contour = contours[0]
     numContourPixels = len(contour)
@@ -28,14 +40,22 @@ def fourier_descriptors_feature(mask):
     for contourPixel in contourPixels:
         complexValues.append(complex(contourPixel[0], contourPixel[1]))
 
-    spectrum = np.fft.fft(complexValues)
+    fullSpectrum = np.fft.fft(complexValues)
+    firstFrequencies = fullSpectrum[0:numfourierDescriptorsToKeep]
+    absSpectrum = [abs(complexValue) for complexValue in firstFrequencies]
+    dropFirstTerm = absSpectrum[1:]
+    dropFirstTermNormalized = [dropFirstTerm[i]/dropFirstTerm[0]
+                               for i in range(len(dropFirstTerm))]
 
-    return contourPixels
+    fdFeatures = {}
+    fdFeatures["fdFeatures"] = dropFirstTermNormalized
+
+    return fdFeatures
 
 
 if __name__ == "__main__":
     fishMasks = masks(False)
-    firstFishMaskPath = fishMasks['mask_01'][76]
+    firstFishMaskPath = fishMasks['mask_01'][75]
     mask = cv2.imread(firstFishMaskPath)
     maskCv2Bw = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
     withContours = fourier_descriptors_feature(maskCv2Bw)
